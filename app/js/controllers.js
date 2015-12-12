fedcd2d421108838936d191fdf018e100cbf1adc
Martin Fedec <martin@amplitudesolutions.ca>
2015-12-12 13:03:53 -0800

    Sidemenu open/close account_setting drop down

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,5 +2,13 @@
 
 angular.module('amplitudeApp.controllers', [])
 
+.controller('menuBarCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+
+	$scope.toggleSidenav = function() {
+		$mdSidenav('left').toggle();
+	}
+
+}])
+
 
 ;
