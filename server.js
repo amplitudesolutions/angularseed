8e4c5ccb51afef39563c9a2b2c7e9c411d6a7c8b
Martin Fedec <martin@amplitudesolutions.ca>
2016-11-11 14:10:46 -0800

    Updated Packages, check readme..

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var gulp = require('gulp');
+require('./gulpfile');
+
+gulp.start('config');
+
+app.use(express.static(__dirname + '/app'));
+
+var port = process.env.PORT || 8000;
+
+app.listen(port);
+
+console.log('Magic happening on port ' + port)
